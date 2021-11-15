2ae87e80978752e7bc1a0d4b0f4e83dd0c6c7543
Jim <jiminbentley@gmail.com>
2021-11-15 18:56:04 -0500

    changes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -19,7 +19,8 @@ const Index = () => (
       </header>
       <p> Welcome to my website. This site was created with React.
       </p>
-      <p>Check out my <a href="https://www.behance.net/jimdoge" target="_blank" rel="noopener noreferrer">work!</a>.</p>
+      <p>Check out my <a href="https://jiminchun.com/works" target="_blank" rel="noopener noreferrer">main work!</a>.</p>
+      <p>Check out my <a href="https://www.behance.net/jimdoge" target="_blank" rel="noopener noreferrer">other work!</a>.</p>
       <p>Check out my <a href="https://www.redbubble.com/people/joomynoomy/shop?asc=u&ref=account-nav-dropdown" target="_blank" rel="noopener noreferrer">store!</a>.</p>
       <p>Check out my <a href="https://www.youtube.com/channel/UCxUgMd_UEbveVjKbOGvgvdA/videos" target="_blank" rel="noopener noreferrer">music!</a>.</p>
     </article>
